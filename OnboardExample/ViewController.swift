9f084669d1bcb587b3ba61d64302700c311a8890
Nikola Kirev <nikola.kirev@gmail.com>
2018-01-29 10:25:21 +0000

    Add assets in example project

--- OnboardExample/ViewController.swift
+++ OnboardExample/ViewController.swift
@@ -9,37 +9,37 @@ import UIKit
 import OnboardKit
 
 class ViewController: UIViewController {
-  
+
   lazy var onboardingSlides: [OnboardPage] = {
-    let slideOne = OnboardPage(title: NSLocalizedString("Welcome to OnboardKit", comment: ""),
-                               imageName: "page1",
+    let slideOne = OnboardPage(title: NSLocalizedString("Welcome to Habitat", comment: ""),
+                               imageName: "Onboarding1",
                                description: NSLocalizedString("Habitat is an easy to use productivity app designed to keep you motivated.", comment: ""))
-    
+
     let slideTwo = OnboardPage(title: NSLocalizedString("Habit Entries", comment: ""),
                                imageName: "Onboarding2",
                                description: NSLocalizedString("For each of your habits an entry is created for every day you need to complete it.", comment: ""))
-    
+
     let slideThree = OnboardPage(title: NSLocalizedString("Marking and Tracking", comment: ""),
                                  imageName: "Onboarding3",
                                  description: NSLocalizedString("By marking entries as Done you can track your progress on the path to success.", comment: ""))
-    
+
     let slideFour = OnboardPage(title: NSLocalizedString("Notifications", comment: ""),
                                 imageName: "Onboarding4",
                                 description: NSLocalizedString("Turn on notifications to get reminders and keep up with your goals.", comment: ""),
                                 actionButtonTitle: NSLocalizedString("Enable Notifications", comment: ""),
                                 advanceButtonTitle: NSLocalizedString("Decide Later", comment: ""))
-    
+
     let slideFive = OnboardPage(title: NSLocalizedString("All Ready", comment: ""),
                                 imageName: "Onboarding5",
                                 description: NSLocalizedString("You are all set up and ready to use Habitat. Begin by adding your first habit.", comment: ""),
                                 advanceButtonTitle: NSLocalizedString("Done", comment: ""))
-    
+
     return [slideOne, slideTwo, slideThree, slideFour, slideFive]
   }()
-  
+
   @IBAction func showOnboardingTapped(_ sender: Any) {
     let onboardingVC = OnboardViewController(slideItems: onboardingSlides)
     onboardingVC.modalPresentationStyle = .formSheet
-    self.present(onboardingVC, animated: true, completion: nil)
+    onboardingVC.presentFrom(self, animated: true)
   }
 }
