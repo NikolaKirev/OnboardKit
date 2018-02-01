3ec5646a334c34d94a3c0571129ddf12d61493d5
Nikola Kirev <NikolaKirev@users.noreply.github.com>
2018-02-01 20:47:31 +0000

    Merge progress from develop (#1)
    * Add assets in example project
    
    * Tidy up example project
    
    * Add doccumenttion for public properties
    
    - Rename “slide” to “page”
    
    * Add demo.gif to README

--- OnboardExample/ViewController.swift
+++ OnboardExample/ViewController.swift
@@ -2,44 +2,60 @@
 //  ViewController.swift
 //  OnboardExample
 //
-//  Created by Nikola Kirev on 22/07/2017.
-//
 
 import UIKit
 import OnboardKit
 
 class ViewController: UIViewController {
-  
-  lazy var onboardingSlides: [OnboardPage] = {
-    let slideOne = OnboardPage(title: NSLocalizedString("Welcome to OnboardKit", comment: ""),
-                               imageName: "page1",
-                               description: NSLocalizedString("Habitat is an easy to use productivity app designed to keep you motivated.", comment: ""))
-    
-    let slideTwo = OnboardPage(title: NSLocalizedString("Habit Entries", comment: ""),
-                               imageName: "Onboarding2",
-                               description: NSLocalizedString("For each of your habits an entry is created for every day you need to complete it.", comment: ""))
-    
-    let slideThree = OnboardPage(title: NSLocalizedString("Marking and Tracking", comment: ""),
-                                 imageName: "Onboarding3",
-                                 description: NSLocalizedString("By marking entries as Done you can track your progress on the path to success.", comment: ""))
-    
-    let slideFour = OnboardPage(title: NSLocalizedString("Notifications", comment: ""),
-                                imageName: "Onboarding4",
-                                description: NSLocalizedString("Turn on notifications to get reminders and keep up with your goals.", comment: ""),
-                                actionButtonTitle: NSLocalizedString("Enable Notifications", comment: ""),
-                                advanceButtonTitle: NSLocalizedString("Decide Later", comment: ""))
-    
-    let slideFive = OnboardPage(title: NSLocalizedString("All Ready", comment: ""),
-                                imageName: "Onboarding5",
-                                description: NSLocalizedString("You are all set up and ready to use Habitat. Begin by adding your first habit.", comment: ""),
-                                advanceButtonTitle: NSLocalizedString("Done", comment: ""))
-    
-    return [slideOne, slideTwo, slideThree, slideFour, slideFive]
+
+  lazy var onboardingPages: [OnboardPage] = {
+    let pageOne = OnboardPage(title: "Welcome to Habitat",
+                              imageName: "Onboarding1",
+                              description: "Habitat is an easy to use productivity app designed to keep you motivated.")
+
+    let pageTwo = OnboardPage(title: "Habit Entries",
+                              imageName: "Onboarding2",
+                              description: "For each of your habits an entry is created for every day you need to complete it.")
+
+    let pageThree = OnboardPage(title: "Marking and Tracking",
+                                imageName: "Onboarding3",
+                                description: "By marking entries as Done you can track your progress on the path to success.")
+
+    let pageFour = OnboardPage(title: "Notifications",
+                               imageName: "Onboarding4",
+                               description: "Turn on notifications to get reminders and keep up with your goals.",
+                               advanceButtonTitle: "Decide Later",
+                               actionButtonTitle: "Enable Notifications",
+                               action: { [weak self] completion in
+                                self?.showAlert(completion)
+    })
+
+    let pageFive = OnboardPage(title: "All Ready",
+                               imageName: "Onboarding5",
+                               description: "You are all set up and ready to use Habitat. Begin by adding your first habit.",
+                               advanceButtonTitle: "Done")
+
+    return [pageOne, pageTwo, pageThree, pageFour, pageFive]
   }()
-  
+
   @IBAction func showOnboardingTapped(_ sender: Any) {
-    let onboardingVC = OnboardViewController(slideItems: onboardingSlides)
+    let onboardingVC = OnboardViewController(pageItems: onboardingPages)
     onboardingVC.modalPresentationStyle = .formSheet
-    self.present(onboardingVC, animated: true, completion: nil)
+    onboardingVC.presentFrom(self, animated: true)
+  }
+
+  /// Only for the purpouses of the example.
+  /// Not really asking for notifications permissions.
+  private func showAlert(_ completion: @escaping (_ success: Bool, _ error: Error?) -> Void) {
+    let alert = UIAlertController(title: "Allow Notifications?",
+                                  message: "Habitat wants to send you notifications",
+                                  preferredStyle: .alert)
+    alert.addAction(UIAlertAction(title: "OK", style: .default) { _ in
+      completion(true, nil)
+    })
+    alert.addAction(UIAlertAction(title: "Cancel", style: .cancel) { _ in
+      completion(false, nil)
+    })
+    presentedViewController?.present(alert, animated: true)
   }
 }
