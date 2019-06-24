5cb7bd48aa188f43402cf09866ea89f411065161
Nikola Kirev <nikola.kirev@gmail.com>
2019-06-24 12:25:50 +0300

    Add min iOS version requirement

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "OnboardKit",
+    platforms: [.iOS(.v11)],
     products: [
         .library(name: "OnboardKit",  targets: ["OnboardKit"])
     ],
